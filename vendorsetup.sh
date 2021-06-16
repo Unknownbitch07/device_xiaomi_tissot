4384fd232a268811b1984a634ba03c009cc26760
Unknownbitch <68426327+Unknownbitch07@users.noreply.github.com>
2021-06-16 22:00:21 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -24,9 +24,14 @@ for var in eng user userdebug; do
   add_lunch_combo omni_tissot-$var
 done
 
+export TARGET_ARCH=arm64
 export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export LC_ALL="C"
 export OF_AB_DEVICE=1
 export FOX_BUGGED_AOSP_ARB_WORKAROUND="1420041600"
 export OF_MAINTAINER="Unknownbitch"
+export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1 
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1 
+export FOX_ADVANCED_SECURITY=1 
